f482d40b879f9a93068c2f3a423dff9023a6a34c
GianniCampana <campanagianni97@gmail.com>
2021-04-13 20:42:21 +0200

    snack

---
+++ snake1/script.js
@@ -0,0 +1,33 @@
+
+/*  
+ var somma = 0;
+
+for(i=0; i<5; i++){
+    var numero = parseInt(prompt("inserisci un numero"));
+    somma += numero++;
+}
+
+document.getElementById("somma-for").innerHTML= "la somma dei tuoi numeri è " + somma;  */
+
+ 
+
+var n = 5; 
+var somma_ = 0;
+var cont = 0;
+
+while(cont < n){
+    var numero = parseInt(prompt("inserisci un numero"));
+    cont++;
+    somma_+=numero;
+}
+
+
+document.getElementById("somma-while").innerHTML= "la somma dei tuoi numeri è " + somma_;
+
+
+
+
+
+
+
+
